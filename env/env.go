51e40507501d89ae614ed921971e537e57b59622
tonnyzhang <wodexintiao@gmail.com>
2024-05-10 20:51:20 +0800

    feat: 对外开放加载环境变量方法

--- env/env.go
+++ env/env.go
@@ -1,18 +1,25 @@
 package env
 
 import (
-	"github.com/tonny-zhang/goutils/fileutils"
 	"path"
+	"sync"
+
+	"github.com/tonny-zhang/goutils/fileutils"
 
 	"github.com/joho/godotenv"
 )
 
-func init() {
-	// 自动加载环境变量
-	var envFiles []string
-	envFile := path.Join(fileutils.GetCmdDir(), ".env")
-	if fileutils.IsFileExists(envFile) {
-		envFiles = append(envFiles, envFile)
-	}
-	godotenv.Overload(envFiles...)
+var locker sync.Once
+
+// AutoLoad 自动加载环境变量
+func AutoLoad() {
+	locker.Do(func() {
+		// 自动加载环境变量
+		var envFiles []string
+		envFile := path.Join(fileutils.GetCmdDir(), ".env")
+		if fileutils.IsFileExists(envFile) {
+			envFiles = append(envFiles, envFile)
+		}
+		godotenv.Overload(envFiles...)
+	})
 }
